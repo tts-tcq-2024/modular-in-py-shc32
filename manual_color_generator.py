d640e8b2100f80b933e9e4ca44469322f01ed16b
shc32 <shc146388@gmail.com>
2024-07-28 16:08:49 +0530

    Update manual_color_generator.py

--- manual_color_generator.py
+++ manual_color_generator.py
@@ -1,10 +1,10 @@
-import color_code_mapping
+import color_code_map
 
 def generate_color_manual():
     manual = []
-    for major_color in color_code.MAJOR_COLORS:
-        for minor_color in color_code.MINOR_COLORS:
-            pair_number = color_code.get_pair_number_from_color(major_color, minor_color)
+    for major_color in color_code_map.MAJOR_COLORS:
+        for minor_color in color_code_map.MINOR_COLORS:
+            pair_number = color_code_map.get_pair_number_from_color(major_color, minor_color)
             manual.append(f'Pair Number {pair_number}: {color_code.color_pair_to_string(major_color, minor_color)}')
     return "\n".join(manual)
 
