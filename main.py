cc4a54d456a229a63d2a05b304e8c591f35fa8b8
shc32 <shc146388@gmail.com>
2024-07-30 16:28:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 import color_code_map
-import color_code_test
-import color_code_manual
 
 if __name__ == '__main__':
-    # test cases
-    test_number_to_pair(4, 'White', 'Brown')
-    test_number_to_pair(5, 'White', 'Slate')
-    test_pair_to_number('Black', 'Orange', 12)
-    test_pair_to_number('Violet', 'Slate', 25)
-    test_pair_to_number('Red', 'Orange', 7)
-    print('All tests passed!')
-
     # color code manual generation 
     print_color_manual()
