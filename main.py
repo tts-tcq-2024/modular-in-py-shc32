3bbbab6e5c9c7268277efc2a7faf2bc142532e37
shc32 <shc146388@gmail.com>
2024-07-30 16:53:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,13 @@ def print_color_manual():
     print(generate_color_manual())
 
 if __name__ == '__main__':
+    # test cases
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('All tests passed!')
+    
     # color code manual generation 
     print_color_manual()
