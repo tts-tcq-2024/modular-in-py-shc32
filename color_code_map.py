69a55b2f875bee354bce37a72429319aee53b82a
shc32 <shc146388@gmail.com>
2024-07-30 16:39:21 +0530

    Update color_code_map.py

--- color_code_map.py
+++ color_code_map.py
@@ -1,9 +1,6 @@
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-def color_pair_to_string(major_color, minor_color):
-    return f'{major_color} {minor_color}'
-
 def get_color_from_pair_number(pair_number):
     if pair_number < 1 or pair_number > len(MAJOR_COLORS) * len(MINOR_COLORS):
         raise ValueError('Pair number out of range')
