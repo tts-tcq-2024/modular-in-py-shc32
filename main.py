a0e7acba47e48f11144fc31ec75b1123582ad1fd
shc32 <shc146388@gmail.com>
2024-07-30 16:23:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import color_code_map
-
-# new functionality added - coding to be printed as a reference manual. This manual is a mapping from the color-names to the corresponding numbers
-def generate_color_manual():
-    manual = []
-    for major_color in color_code_map.MAJOR_COLORS:
-        for minor_color in color_code_map.MINOR_COLORS:
-            pair_number = color_code_map.get_pair_number_from_color(major_color, minor_color)
-            manual.append(f'Pair Number {pair_number}: {color_code_map.color_pair_to_string(major_color, minor_color)}')
-    return "\n".join(manual)
-
-def print_color_manual():
-    print(generate_color_manual())
+import color_code_test
 
 if __name__ == '__main__':
+    # test cases
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('All tests passed!')
+
+    # color code manual generation 
     print_color_manual()
