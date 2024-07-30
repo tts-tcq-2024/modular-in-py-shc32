74613760d8bcadf3bb966e158120064cc76c8aed
shc32 <shc146388@gmail.com>
2024-07-30 16:29:03 +0530

    Update color_code_test.py

--- color_code_test.py
+++ color_code_test.py
@@ -8,3 +8,10 @@ def test_number_to_pair(pair_number, expected_major_color, expected_minor_color)
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
     pair_number = color_code_map.get_pair_number_from_color(major_color, minor_color)
     assert pair_number == expected_pair_number, f'Expected {expected_pair_number}, got {pair_number}'
+
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print('All tests passed!')
