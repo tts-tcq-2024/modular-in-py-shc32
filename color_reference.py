105ffab6d34553e434e8102d50fece2125ed178e
shc32 <shc146388@gmail.com>
2024-07-30 17:51:20 +0530

    Update color_reference.py

--- color_reference.py
+++ color_reference.py
@@ -3,10 +3,10 @@ from color_coding_utility import MAJOR_COLORS, MINOR_COLORS, color_pair_to_strin
 # new functionality added - coding to be printed as a reference manual. This manual is a mapping from the color-names to the corresponding numbers
 def generate_color_manual():
     manual = []
-    for major_color in MAJOR_COLORS:
-        for minor_color in MINOR_COLORS:
-            pair_number = get_pair_number_from_color(major_color, minor_color)
-            manual.append(f'Pair Number {pair_number}: {color_pair_to_string(major_color, minor_color)}')
+    for major_color in color_coding_utility.MAJOR_COLORS:
+        for minor_color in color_coding_utility.MINOR_COLORS:
+            pair_number = color_coding_utility.get_pair_number_from_color(major_color, minor_color)
+            manual.append(f'Pair Number {pair_number}: {color_coding_utility.color_pair_to_string(major_color, minor_color)}')
     return "\n".join(manual)
 
 def print_color_manual():
